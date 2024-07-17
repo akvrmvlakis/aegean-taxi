affa2e7a071e76684b8d24bc348f90410a7571b0
Alexandros Kourmoulakis <pro_balla2000@hotmail.com>
2024-07-17 16:11:12 +0300

    modify flex properties and text columns on vehicle selection page

--- src/SelectTaxi.jsx
+++ src/SelectTaxi.jsx
@@ -73,73 +73,126 @@ const SelectTaxi = () => {
           </Map>
         </APIProvider>
       </div>
-      <div className="w-full mx-auto bg-white px-4 my-4 rounded-lg">
+      <div className="flex items-center justify-center">
+        <p className="text-xl font-semibold mt-4 text-[#264388]">
+          Choose a trip
+        </p>
+      </div>
+      <div className="w-full mx-auto bg-white px-4 my-4 rounded-2xl">
         <div className="space-y-4">
           <div
-            className={`py-1 px-4 border-2 rounded-xl cursor-pointer flex items-center justify-between ${
+            className={`py-1 px-4 border-[3px] rounded-2xl cursor-pointer flex items-center justify-between ${
               selectedItem === "item1"
                 ? "border-[#264388]"
                 : "border-transparent"
             }`}
             onClick={() => handleSelect("item1")}
           >
-            <img src={uberCar} alt="Taxi Icon" className="w-[60px] h-[60px]" />
-            <div className="flex flex-col items-center justify-start">
-              <p className="text-[#264388] font-bold text-xl">Economy</p>
-              <div className="flex items-center justify-start">
-                <img
-                  src={personIcon}
-                  alt="Person Icon"
-                  className="w-[15px] h-[15px] mr-2"
-                />
-                <span className="text-base italic font-semibold">1 - 4</span>
+            <div className="flex flex-row items-center justify-start">
+              <img
+                src={uberCar}
+                alt="Taxi Icon"
+                className="w-[75px] h-[75px] mr-2"
+              />
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex flex-row items-center justify-start">
+                  <p className="text-[#264388] font-bold text-lg mx-1">
+                    Economy
+                  </p>
+                  <img
+                    src={personIcon}
+                    alt="Person Icon"
+                    className="w-[15px] h-[15px] mr-1"
+                  />
+                  <span className="text-base font-normal">4</span>
+                </div>
+                <div className="flex flex-row items-center justify-between">
+                  <p className="text-sm mx-1 font-semibold">
+                    <span>15:25</span>
+                  </p>
+                  {/* <div className="text-3xl leading-0">.</div> */}
+                  <p className="text-sm mx-1 font-semibold">
+                    <span>3 mins away</span>
+                  </p>
+                </div>
               </div>
             </div>
             <p className="font-bold text-[#264388] text-2xl">€36</p>
           </div>
           <div
-            className={`py-1 px-4 border-2 rounded-xl cursor-pointer flex items-center justify-between ${
+            className={`py-1 px-4 border-[3px] rounded-xl cursor-pointer flex items-center justify-between ${
               selectedItem === "item2"
                 ? "border-[#264388]"
                 : "border-transparent"
             }`}
             onClick={() => handleSelect("item2")}
           >
-            <img src={uberVan} alt="Taxi Icon" className="w-[60px] h-[60px]" />
-            <div className="flex flex-col items-center justify-start">
-              <p className="text-[#264388] font-bold text-xl">Van</p>
-              <div className="flex items-center justify-start">
-                <img
-                  src={personIcon}
-                  alt="Person Icon"
-                  className="w-[15px] h-[15px] mr-2"
-                />
-                <span className="text-base italic font-semibold">1 - 4</span>
+            <div className="flex flex-row items-center justify-start">
+              <img
+                src={uberVan}
+                alt="Taxi Icon"
+                className="w-[75px] h-[75px] mr-2"
+              />
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex flex-row items-center justify-start">
+                  <p className="text-[#264388] font-bold text-lg mx-1">Van</p>
+                  <img
+                    src={personIcon}
+                    alt="Person Icon"
+                    className="w-[15px] h-[15px] mr-1"
+                  />
+                  <span className="text-base font-normal">7</span>
+                </div>
+                <div className="flex flex-row items-center justify-between">
+                  <p className="text-sm mx-1 font-semibold">
+                    <span>15:25</span>
+                  </p>
+                  <p className="text-sm mx-1 font-semibold">
+                    <span>3 mins away</span>
+                  </p>
+                </div>
               </div>
             </div>
-            <p className="font-bold text-[#264388] text-2xl">€45</p>
+            <p className="font-bold text-[#264388] text-2xl">€36</p>
           </div>
           <div
-            className={`py-1 px-4 border-2 rounded-xl cursor-pointer flex items-center justify-between ${
+            className={`py-1 px-4 border-[3px] rounded-xl cursor-pointer flex items-center justify-between ${
               selectedItem === "item3"
                 ? "border-[#264388]"
                 : "border-transparent"
             }`}
             onClick={() => handleSelect("item3")}
           >
-            <img src={uberVan} alt="Taxi Icon" className="w-[60px] h-[60px]" />
-            <div className="flex flex-col items-center justify-start">
-              <p className="text-[#264388] font-bold text-xl">Mini Bus</p>
-              <div className="flex items-center justify-start">
-                <img
-                  src={personIcon}
-                  alt="Person Icon"
-                  className="w-[15px] h-[15px] mr-2"
-                />
-                <span className="text-base italic font-semibold">1 - 4</span>
+            <div className="flex flex-row items-center justify-start">
+              <img
+                src={uberVan}
+                alt="Taxi Icon"
+                className="w-[75px] h-[75px] mr-2"
+              />
+              <div className="flex flex-col items-start justify-start">
+                <div className="flex flex-row items-center justify-start">
+                  <p className="text-[#264388] font-bold text-lg mx-1">
+                    Mini Bus
+                  </p>
+                  <img
+                    src={personIcon}
+                    alt="Person Icon"
+                    className="w-[15px] h-[15px] mr-1"
+                  />
+                  <span className="text-base font-normal">12</span>
+                </div>
+                <div className="flex flex-row items-center justify-between">
+                  <p className="text-sm mx-1 font-semibold">
+                    <span>15:25</span>
+                  </p>
+                  {/* <div className="text-3xl leading-0">.</div> */}
+                  <p className="text-sm mx-1 font-semibold">
+                    <span>3 mins away</span>
+                  </p>
+                </div>
               </div>
             </div>
-            <p className="font-bold text-[#264388] text-2xl">€45</p>
+            <p className="font-bold text-[#264388] text-2xl">€36</p>
           </div>
         </div>
       </div>
